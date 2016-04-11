27ac0cd226a0de68ec388d193a4240fd5caa6ca8
adikrn <adikrn@users.noreply.github.com>
2016-04-11 11:22:43 +0530

    completed assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,14 @@
 ## Caching the inverse of a matrix:
 ## The function 'makeCacheMatrix()' creates a special matrix objects that 
-## can cache its inverse and return it when asked for. SImilarly, 
+## can cache its inverse and return it when asked for. Similarly, 
 ## the function 'cacheSolve()' first checks whether the matrix inverse has
 ## already been computed and cached or not. If yes, it retrieves the
-## cached inverse and returns it else, it computes the inverse, caches it ## and then, return the inverse.
+## cached inverse and returns it else, it computes the inverse, caches it
+## and then, return the inverse.
 
 ## The function 'makeCacheMatrix()' creates a special matrix objects that 
 ## can cache its inverse and return it when asked for. The function 'set()' 
-## sets the matrix data in the special object. SImilarly, 'setinverse()' 
+## sets the matrix data in the special object. Similarly, 'setinverse()' 
 ## caches the inverse provided as argument. Functions 'get()' and 
 ## 'getInverse()' return matrix data and cached inverse of the matrix
 ## respectively.   
